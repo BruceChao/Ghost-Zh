906c9afa7014c1bf8f60f03f7df43f95fba0ce33
Bruce Chao <bornichildren@gmail.com>
2015-10-01 03:42:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 // # Ghost Configuration
-// Setup your Ghost install for various environments
-// Documentation can be found at http://support.ghost.org/config/
+// Setup your Ghost install for various [environments](http://support.ghost.org/config/#about-environments).
+
+// Ghost runs in `development` mode by default. Full documentation can be found at http://support.ghost.org/config/
 
 var path = require('path'),
     config;
 
 config = {
     // ### Production
-    // When running Ghost in the wild, use the production environment
+    // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
         url: 'http://ibruce.me',
@@ -20,19 +21,71 @@ config = {
             debug: false
         },
 
+        // 配置MySQL 数据库
+        /*database: {
+            client: 'mysql',
+            connection: {
+                host     : 'host',
+                user     : 'user',
+                password : 'password',
+                database : 'database',
+                charset  : 'utf8'
+            },
+            debug: false
+        },*/
+
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        },
+        //Storage.Now,we can support `qiniu`,`upyun`, `aliyun oss`, `aliyun ace-storage` and `local-file-store`
+        storage: {
+            provider: 'local-file-store'
         }
+
+        // or
+        // 参考文档： http://www.ghostchina.com/qiniu-cdn-for-ghost/
+        /*storage: {
+            provider: 'qiniu',
+            bucketname: 'your-bucket-name',
+            ACCESS_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            SECRET_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            root: '/image/',
+            prefix: 'http://your-bucket-name.qiniudn.com'
+        }*/
+
+        // or
+        // 参考文档： http://www.ghostchina.com/upyun-cdn-for-ghost/
+        /*storage: {
+            provider: 'upyun',
+            bucketname: 'your-bucket-name',
+            username: 'your user name',
+            password: 'your password',
+            root: '/image/',
+            prefix: 'http://your-bucket-name.b0.upaiyun.com'
+        }*/
+
+        // or
+        // 参考文档： http://www.ghostchina.com/aliyun-oss-for-ghost/
+        /*storage: {
+            provider: 'oss',
+            bucketname: 'your-bucket-name',
+            ACCESS_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            SECRET_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            root: '/image/',
+            endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',  //阿里云的上传端点是分地域的，需要单独设置
+            prefix: 'http://your-bucket-name.oss-cn-hangzhou.aliyuncs.com'
+        }*/
     },
 
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        // Change this to your Ghost blogs published URL.
-        url: 'http://localhost:2368',
+        // Change this to your Ghost blog's published URL.
+        url: 'http://ibruce.me',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
@@ -49,6 +102,8 @@ config = {
         //  },
         // ```
 
+        // #### Database
+        // Ghost supports sqlite3 (default), MySQL & PostgreSQL
         database: {
             client: 'sqlite3',
             connection: {
@@ -56,12 +111,16 @@ config = {
             },
             debug: false
         },
+        // #### Server
+        // Can be host & port (default), or socket
         server: {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         },
+        // #### Paths
+        // Specify where your content directory lives
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
@@ -130,5 +189,4 @@ config = {
     }
 };
 
-// Export config
 module.exports = config;
