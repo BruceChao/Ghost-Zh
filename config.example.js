f332497e8d178d5f356de0ef946b128e10478824
Bruce Chao <bornichildren@gmail.com>
2015-10-01 18:38:03 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -13,14 +13,6 @@ config = {
     production: {
         url: 'http://my-ghost-blog.com',
         mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-
         // 配置MySQL 数据库
         *database: {
             client: 'mysql',
